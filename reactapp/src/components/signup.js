dc49dba4838d15b7421d2542a00fe224e5c3d552
Sai Srujan Penta <98844510+saisrujanpenta@users.noreply.github.com>
2022-12-08 16:53:12 -0500

    Added alert message

--- reactapp/src/components/signup.js
+++ reactapp/src/components/signup.js
@@ -34,6 +34,7 @@ export default class SignUp extends Component {
       .then((data) => {
         console.log(data, "userRegister");
         alert("User registration successful");
+        alert("Click on Login now!");
       });
   }
   render() {
