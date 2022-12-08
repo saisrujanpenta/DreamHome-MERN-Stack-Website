16b640ded606966f83c86a24456723857ff93582
Geetanjali K <kamble.ge@northeastern.edu>
2022-12-08 14:54:25 -0500

    App css

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-//comment
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from './components/login';
+import Signup from './components/signup';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FeaturedProjects from "./DB/FeaturedProjects";
+
+
+
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Signup />
+
     </div>
+
   );
+
 }
+
+
 
 export default App;
