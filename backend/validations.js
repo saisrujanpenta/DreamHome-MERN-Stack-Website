adf85c68b7f67a3fee1ec95b9f3ca38c3f1ef541
Madhulika23 <dekate.ma@northeastern.edu>
2022-12-08 12:39:38 -0500

    updated validation

--- backend/validations.js
+++ backend/validations.js
@@ -1,4 +1,4 @@
-
+// validations of name for login/registration page
 const validateName = (name) => {
     if (/^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/.test(name)) {
       return (true)
@@ -6,7 +6,7 @@ const validateName = (name) => {
   
     return (false)
   }
-  
+  // validations of mail for login/registration page
   const validateEmail = (mail) => {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
       return (true)
@@ -14,7 +14,7 @@ const validateName = (name) => {
   
     return (false)
   }
-  
+  // validations of password textarea for login/registration page
   const checkPassword = (inputtxt) => {
     var passw = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{4,}$)/;
     if (inputtxt.match(passw)) {
